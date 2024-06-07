6bf94043f8b37b6e6cce68f59b187ebbb6e7f4a2
PC_ALEXIS\alexis <alexis.bessac@gmail.com>
2024-06-07 15:29:32 +0200

    Suite du projet Admin en Angular

--- src/app/ajout-retard/ajout-retard.component.ts
+++ src/app/ajout-retard/ajout-retard.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatSelectModule} from '@angular/material/select';
+import {MatRadioModule} from '@angular/material/radio';
 
 @Component({
   selector: 'app-ajout-retard',
   standalone: true,
   imports: [MatButtonModule,
             MatFormFieldModule,
-            MatSelectModule],
+            MatSelectModule,
+            MatRadioModule],
   templateUrl: './ajout-retard.component.html',
   styleUrl: './ajout-retard.component.scss'
 })
