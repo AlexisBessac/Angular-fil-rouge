1f3429ec4437d40d3d799df81246d8665b7f192e
PC_ALEXIS\alexis <alexis.bessac@gmail.com>
2024-06-07 16:50:50 +0200

    Nouvelle suite du projet Admin en Angular

--- src/app/ajout-retard/ajout-retard.component.ts
+++ src/app/ajout-retard/ajout-retard.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import {MatButtonModule} from '@angular/material/button';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatSelectModule} from '@angular/material/select';
 import {MatRadioModule} from '@angular/material/radio';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-ajout-retard',
@@ -12,7 +15,10 @@ import {MatRadioModule} from '@angular/material/radio';
             MatButtonModule,
             MatFormFieldModule,
             MatSelectModule,
-            MatRadioModule],
+            MatRadioModule, 
+            HttpClientModule,
+            ReactiveFormsModule,
+            FormsModule],
   templateUrl: './ajout-retard.component.html',
   styleUrl: './ajout-retard.component.scss'
 })
@@ -20,7 +26,33 @@ export class AjoutRetardComponent {
   users: any[] = [];
   selectedUser: any;
 
+  formBuilder: FormBuilder = inject(FormBuilder);
+
+  http:HttpClient = inject(HttpClient)
+
+  formulaire: FormGroup = this.formBuilder.group({
+    date: ['', [Validators.required]],
+    listeEtudiant: ['', [Validators.required]],
+  });
+
  ngOnInit(){
+  
+  const jwt = localStorage.getItem('jwt');
+  
+  if (jwt) {
+    this.http
+      .get(
+        'http://backendfilrouge/get-retard.php',
+        { headers: { Authorization: jwt } }
+      )
+      .subscribe({
+        next: (utilisateur) => this.formulaire.patchValue(utilisateur),
+        error: (erreur) => alert(erreur.error.message),
+      });
+  }
+ }
+
+ onAjoutRetard(){
 
  }
 }
